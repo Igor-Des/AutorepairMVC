a7b0402f638933ebf83bf5c61c1e23f01d2dc11d
Igor Drapeza <desnigor@mail.ru>
2022-10-23 20:24:01 +0300

    Добавьте файлы проекта.

---
+++ AutorepairMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AutorepairMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AutorepairMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
