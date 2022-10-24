d121f6df9dade08512dcd825b165bfbe13a8e94e
Igor Drapeza <desnigor@mail.ru>
2022-10-24 23:49:44 +0300

    add models &  data context

---
+++ AutorepairMVC/Data/AutorepairContext.cs
@@ -0,0 +1,20 @@
+﻿using AutorepairShopMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AutorepairShopMVC.Data
+{
+    public class AutorepairContext : DbContext
+    {
+        public AutorepairContext(DbContextOptions<AutorepairContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Qualification> Qualifications { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Mechanic> Mechanics { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+    }
+}
