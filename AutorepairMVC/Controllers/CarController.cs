178ff60665af6612e1b0ff5cd13ac98f89af3790
Igor Drapeza <idrapeza@edu.gstu.by>
2022-10-31 10:49:07 +0300

    change car controller to simple car controller

---
+++ AutorepairMVC/Controllers/CarController.cs
@@ -0,0 +1,37 @@
+﻿using AutorepairMVC.Data;
+using AutorepairMVC.Models;
+using AutorepairMVC.ViewModels;
+using FuelStation.Infrastructure.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AutorepairMVC.Controllers
+{
+    [ExceptionFilter]
+    [TypeFilter(typeof(TimingLogAttribute))]
+    public class CarController : Controller
+    {
+        private readonly AutorepairContext _db;
+        public CarController(AutorepairContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            int numberRows = 15;
+
+            List<Owner> owners = _db.Owners.Take(numberRows).ToList();
+            List<Car> cars = _db.Cars.Take(numberRows).ToList();
+
+
+            CarViewModel carViewModel = new CarViewModel
+            {
+                Owners = owners,
+                Cars = cars
+            };
+            return View(carViewModel);
+        }
+
+    }
+}
---
+++ AutorepairMVC/ViewModels/CarViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AutorepairMVC.Models;
+using System.Threading.Tasks;
+
+namespace AutorepairMVC.ViewModels
+{
+    public class CarViewModel {
+        public IEnumerable<Car> Cars { get; set; }
+        public IEnumerable<Owner> Owners { get; set; }
+    }
+}
